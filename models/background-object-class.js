4039b27f0bfa49dd6c5ab6f1f1e7c847b9770a4e
normanrahmel <92368615+normanrahmel@users.noreply.github.com>
2022-06-27 10:33:48 +0200

    backgroundObjects y =0

--- models/background-object-class.js
+++ models/background-object-class.js
@@ -12,6 +12,6 @@ class backgroundObjects extends MovableObject {
     constructor(imagePath, x) {
         super().loadImage(imagePath);
         this.x = x;
-        this.y = 480 - this.height;
+        this.y = 0;
     }
 }
