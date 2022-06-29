e70476a048ec03b289f479d43c0cbbe0e33278ed
normanrahmel <92368615+normanrahmel@users.noreply.github.com>
2022-06-29 15:09:03 +0200

    implement jump function

--- models/character.class.js
+++ models/character.class.js
@@ -13,6 +13,18 @@ class Character extends MovableObject {
         './img/2_character_pepe/2_walk/W-25.png',
         './img/2_character_pepe/2_walk/W-26.png'
     ];
+    IMAGES_JUMPING = [
+        'img/2_character_pepe/3_jump/J-31.png',
+        'img/2_character_pepe/3_jump/J-32.png',
+        'img/2_character_pepe/3_jump/J-33.png',
+        'img/2_character_pepe/3_jump/J-34.png',
+        'img/2_character_pepe/3_jump/J-35.png',
+        'img/2_character_pepe/3_jump/J-36.png',
+        'img/2_character_pepe/3_jump/J-37.png',
+        'img/2_character_pepe/3_jump/J-38.png',
+        'img/2_character_pepe/3_jump/J-39.png'
+
+    ];
 
     walking_sound = new Audio('audio/running.mp3'); //Walking sound. The Mp3 file is not so good i need to look for a better one.
 
@@ -20,6 +32,7 @@ class Character extends MovableObject {
     constructor() {
         super().loadImage('./img/2_character_pepe/2_walk/W-21.png');
         this.loadImages(this.IMAGES_WALKING);
+        this.loadImages(this.IMAGES_JUMPING);
         this.applyGravity();
         this.animate();
     }
@@ -37,14 +50,22 @@ class Character extends MovableObject {
                 this.otherDirection = true;
                 this.walking_sound.play();
             }
+            if (this.World.keyboard.UP) {
+                this.speedY = 20;
+            }
+
             this.World.camera_x = -this.x + 100;
         }, 1000 / 60);
 
         setInterval(() => {
 
-            if (this.World.keyboard.RIGHT || this.World.keyboard.LEFT) {
-                //Walk animation is in the MO Defined
-                this.playAnimation(this.IMAGES_WALKING);
+            if (this.isAboveGround()) {
+                this.playAnimation(this.IMAGES_JUMPING)
+            } else {
+                if (this.World.keyboard.RIGHT || this.World.keyboard.LEFT) {
+                    //Walk animation is in the MO Defined
+                    this.playAnimation(this.IMAGES_WALKING);
+                }
             }
         }, 50);
     }
