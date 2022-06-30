82b4786fe4d6155892e63d2fd1d6b955f94936c9
normanrahmel <92368615+normanrahmel@users.noreply.github.com>
2022-06-30 14:57:09 +0200

    statusBar

--- models/status-bar.class.js
+++ models/status-bar.class.js
@@ -2,4 +2,39 @@ class StatusBar {
     /**
      * Global variables for the class
      */
+    IAMGES = [
+        'img/7_statusbars/1_statusbar/2_statusbar_health/green/0.png', //0
+        'img/7_statusbars/1_statusbar/2_statusbar_health/green/20.png',
+        'img/7_statusbars/1_statusbar/2_statusbar_health/green/40.png',
+        'img/7_statusbars/1_statusbar/2_statusbar_health/green/80.png',
+        'img/7_statusbars/1_statusbar/2_statusbar_health/green/100.png' //5
+    ];
+    percentage = 100;
+
+    constructor() {
+        this.loadImages(this.IAMGES);
+    }
+
+    //setPercentage(50);
+    setPercentage() {
+        this.percentage = percentage; // Number beetween 0...5
+        let path = this.IAMGES[this.resolveImageIndex()];
+        this.img = this.imageCache[path];
+
+        resolveImageIndex() {
+            if (this.percentage == 100) {
+                return 5;
+            } else if (this.percentage > 80) {
+                return 4;
+            } else if (this.percentage > 60) {
+                return 3;
+            } else if (this.percentage > 40) {
+                return 2;
+            } else if (this.percentage > 20) {
+                return 1;
+            } else {
+                return 0;
+            }
+        }
+    }
 }
